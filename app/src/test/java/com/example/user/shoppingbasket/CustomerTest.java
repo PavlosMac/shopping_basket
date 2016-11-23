15622e0c735753735be9f87512c8dfe3e18ca9a7
Pavlos <pavloskoz@hotmail.com>
2016-11-23 13:09:43 +0000

    first commit

---
+++ app/src/test/java/com/example/user/shoppingbasket/CustomerTest.java
@@ -0,0 +1,27 @@
+package com.example.user.shoppingbasket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by user on 23/11/2016.
+ */
+
+public class CustomerTest {
+
+
+    @Before
+    public before(){
+
+        Customer customer = new Customer("Henry", boolean loyaltyCard)
+    }
+
+
+
+    @Test
+    public void testCustomerHasName(){
+        assertEquals("Henry", customer.getName);
+    }
+
+
+}
